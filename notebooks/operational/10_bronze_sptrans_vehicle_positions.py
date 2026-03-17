87e2d6286eb1e6c6de6fce51713e035ee4a76e70
Leandro Santos <leandrolps@gmail.com>
2026-03-17 20:21:38 -0300

    refactor: convert databricks notebooks from ipynb to python scripts

---
+++ notebooks/operational/10_bronze_sptrans_vehicle_positions.py
@@ -0,0 +1,62 @@
+# MAGIC %run ./00_adls_gen2_oauth_connection
+
+storage_account = "stspmobilitydev001"
+
+landing_path = f"abfss://landing@{storage_account}.dfs.core.windows.net/sptrans/vehicle_positions"
+bronze_path = f"abfss://bronze@{storage_account}.dfs.core.windows.net/sptrans/vehicle_positions"
+
+dbutils.fs.mkdirs(bronze_path)
+
+print("Landing:", landing_path)
+print("Bronze:", bronze_path)
+
+df_raw = spark.read.json(landing_path)
+
+display(df_raw)
+
+from pyspark.sql.functions import explode
+
+df_lines = df_raw.select(
+    "hr",
+    explode("l").alias("line")
+)
+
+from pyspark.sql.functions import col, explode
+
+df_vehicles = df_lines.select(
+    col("hr"),
+    col("line.c").alias("line_code"),
+    col("line.cl").alias("line_id"),
+    col("line.lt0").alias("line_name"),
+    explode(col("line.vs")).alias("vehicle")
+)
+
+from pyspark.sql.functions import current_timestamp
+
+df_bronze = df_vehicles.select(
+    "hr",
+    "line_code",
+    "line_id",
+    "line_name",
+    col("vehicle.p").alias("vehicle_prefix"),
+    col("vehicle.a").alias("accessible"),
+    col("vehicle.ta").alias("timestamp_api"),
+    col("vehicle.py").alias("latitude"),
+    col("vehicle.px").alias("longitude")
+).withColumn(
+    "ingestion_timestamp",
+    current_timestamp()
+)
+
+display(df_bronze)
+
+df_bronze.write \
+    .format("delta") \
+    .mode("append") \
+    .save(bronze_path)
+
+print("Dados gravados na Bronze")
+
+df_check = spark.read.format("delta").load(bronze_path)
+
+display(df_check)
---
+++ notebooks/operational/12_gold_sptrans_analytics.py
@@ -0,0 +1,40 @@
+from pyspark.sql.functions import *
+
+silver_path = "abfss://silver@stspmobilitydev001.dfs.core.windows.net/sptrans/vehicle_positions"
+gold_path = "abfss://gold@stspmobilitydev001.dfs.core.windows.net/sptrans/vehicle_positions"
+
+df_silver = spark.read.format("delta").load(silver_path)
+
+display(df_silver)
+
+df_gold_line_activity = (
+    df_silver
+    .groupBy("line_code", "line_name", "event_date", "event_hour")
+    .agg(
+        countDistinct("vehicle_prefix").alias("active_vehicles")
+    )
+)
+
+df_gold_geo = (
+    df_silver
+    .groupBy("line_code", "line_name")
+    .agg(
+        avg("latitude").alias("avg_latitude"),
+        avg("longitude").alias("avg_longitude"),
+        countDistinct("vehicle_prefix").alias("fleet_size")
+    )
+)
+
+df_gold_line_activity.write \
+.format("delta") \
+.mode("overwrite") \
+.save(f"{gold_path}/line_activity")
+
+df_gold_geo.write \
+.format("delta") \
+.mode("overwrite") \
+.save(f"{gold_path}/line_geo")
+
+df_check = spark.read.format("delta").load(f"{gold_path}/line_activity")
+
+display(df_check)
