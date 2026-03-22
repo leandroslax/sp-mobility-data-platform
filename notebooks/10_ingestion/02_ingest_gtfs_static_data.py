511ceaa0af8e27254dcde5d5a9431556ba4610f6
leandroslax <leandrolps@gmail.com>
2026-03-22 21:02:52 +0000

    fix ingest

--- notebooks/10_ingestion/02_ingest_gtfs_static_data.py
+++ notebooks/10_ingestion/02_ingest_gtfs_static_data.py
@@ -1,16 +1,17 @@
 # Databricks notebook source
+# MAGIC %run ../00_setup/00_config
 
 # COMMAND ----------
-%run ../00_setup/00_config
 
-# COMMAND ----------
 print("🚀 Starting GTFS ingestion (FINAL OPTIMIZED)...")
 
 # COMMAND ----------
+
 import zipfile
 import os
 
 # COMMAND ----------
+
 # Paths
 storage_account = account_name
 container = container_bronze
@@ -23,11 +24,13 @@ extract_path = "/tmp/gtfs"
 adls_extract_path = f"{base_path}/gtfs/extracted"
 
 # COMMAND ----------
+
 print("📥 Copying ZIP...")
 
 dbutils.fs.cp(zip_path, f"file:{local_zip}", True)
 
 # COMMAND ----------
+
 print("📦 Extracting ZIP...")
 
 os.makedirs(extract_path, exist_ok=True)
@@ -36,6 +39,7 @@ with zipfile.ZipFile(local_zip, 'r') as zip_ref:
     zip_ref.extractall(extract_path)
 
 # COMMAND ----------
+
 print("⬆️ Moving files to ADLS...")
 
 files = os.listdir(extract_path)
@@ -44,6 +48,7 @@ for f in files:
     dbutils.fs.cp(f"file:{extract_path}/{f}", f"{adls_extract_path}/{f}", True)
 
 # COMMAND ----------
+
 print("📥 Reading from ADLS (distributed)...")
 
 df = spark.read.option("header", True).csv(f"{adls_extract_path}/*.txt")
@@ -51,6 +56,7 @@ df = spark.read.option("header", True).csv(f"{adls_extract_path}/*.txt")
 display(df)
 
 # COMMAND ----------
+
 delta_path = f"{base_path}/gtfs/delta"
 
 df.write \
@@ -59,4 +65,5 @@ df.write \
   .save(delta_path)
 
 # COMMAND ----------
+
 print("✅ GTFS ingestion completed FAST!")
