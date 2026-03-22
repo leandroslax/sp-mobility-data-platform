d4d04bc5a3777ab5d4104410c567c7138cbe24f4
Leandro Santos <leandrolps@gmail.com>
2026-03-22 17:18:29 -0300

    fix: convert all notebooks to Databricks notebook format

--- notebooks/00_setup/00_config.py
+++ notebooks/00_setup/00_config.py
@@ -2,4 +2,19 @@
 storage_account = "stspmobilitydev001"
 container = "bronze"
 
+<<<<<<< HEAD
 account_fqdn = f"{storage_account}.dfs.core.windows.net"
+=======
+# Databricks notebook source
+
+
+# COMMAND ----------
+
+# ==============================
+# GLOBAL CONFIG
+# ==============================
+
+
+
+print("✅ Global config loaded")
+>>>>>>> 2bb055f (fix: convert all notebooks to Databricks notebook format)
--- notebooks/20_bronze/10_bronze_sptrans_vehicle_positions.py
+++ notebooks/20_bronze/10_bronze_sptrans_vehicle_positions.py
@@ -1,3 +1,5 @@
+# Databricks notebook source
+
 # Databricks
 %run ../00_setup/00_config
 
